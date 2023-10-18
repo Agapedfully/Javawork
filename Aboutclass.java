54f4b033e79dbadf3a113a70720d8bfbb6bab556
Agapedfully <134058517+Agapedfully@users.noreply.github.com>
2023-10-18 16:43:09 +0300

    this is the first commit

--- Aboutclass.java
+++ Aboutclass.java
@@ -1,9 +1,32 @@
 package objectandclass;
-
+import java.util.Scanner;
+class book{
+	String title;
+	String author;
+	int numberOfPages;
+}
 public class Aboutclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+	    //creating object
+		book b=new book();
+		//prompt user to enter book details
+		System.out.println("Please enter the title of the book");
+	    b.title=input.next();
+	    System.out.println("Please enter the author of the book");
+	    b.author=input.next();
+	    System.out.println("Please enter the number of pages");
+	    b.numberOfPages=input.nextInt();
+	    
+	    //Display the given information
+	    System.out.println("The book title is: "+b.title );
+	    System.out.println("The book author is: "+b.author );
+	    System.out.println("The book has "+b.numberOfPages+" pages" );
+	    
+	    
+	    
+	
 
 	}
 
